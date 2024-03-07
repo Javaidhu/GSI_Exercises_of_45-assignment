91984191432fdd4f4e12bb3d0532d5e08f7bd97e
Javaid Hussain <159624598+Javaidhu@users.noreply.github.com>
2024-03-07 17:56:19 +0500

    Add files via upload

---
+++ 5.Famous-quote_2.js
@@ -0,0 +1,4 @@
+"use strict";
+let famous_person = 'QuaideAzam';
+let message = '"Unity,Faith and Discipline."';
+console.log(`${famous_person} once said,${message}`);
---
+++ 5.Famous-quote_2.ts
@@ -0,0 +1,3 @@
+let famous_person : string = 'QuaideAzam';
+let message : string = '"Unity,Faith and Discipline."';
+console.log(`${famous_person} once said,${message}`);
