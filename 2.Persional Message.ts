7eac171e4429787c13bda99ad916ff9b58435f1e
Javaid Hussain <159624598+Javaidhu@users.noreply.github.com>
2024-03-07 17:53:59 +0500

    Add files via upload

---
+++ 2.Persional Message.ts
@@ -0,0 +1,7 @@
+let personName: string='';
+personName = prompt('what is your name?')//'';
+if('PersonName !--null && PersonName!--'){
+alert('Hello ${personname} would you like to learn some Python today?')}
+else {
+    ('you have to fill your name!')
+}
---
+++ 2.persional Message.js
@@ -0,0 +1,8 @@
+var personName = '';
+personName = prompt('what is your name?'); //'';
+if ('PersonName !--null && PersonName!--') {
+    alert('Hello ${personname} would you like to learn some Python today?');
+}
+else {
+    ('you have to fill your name!');
+}
